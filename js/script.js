c058907a518c5d29b8136023d7ff5d36936aafbc
yuridido <yuridido@yahoo.it>
2020-07-09 14:58:31 +0200

    dadi

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
-// VARIABILI
-var listaMail = [
-    "prova@mail.it",
-    "sempronio@mail.it",
-    "caio@mail.it",
-    "tizio@mail.it",
-    "yuri@mail.it",
-    "pinco@mail.it",
-    "pallino@mail.it"];
-
-var mailUtente = prompt('inserisci la tua mail');
-// console.log(mailUtente)
-
-document.getElementById('messaggio').innerHTML= "accesso non consentito"
-
-for (var i=0; i <= (listaMail.length -1); i++)
-{
-    if (mailUtente === listaMail[i]) {
-        document.getElementById('messaggio').innerHTML= "accesso consentito"
-    }
-}
+// // VARIABILI
+// var listaMail = [
+//     "prova@mail.it",
+//     "sempronio@mail.it",
+//     "caio@mail.it",
+//     "tizio@mail.it",
+//     "yuri@mail.it",
+//     "pinco@mail.it",
+//     "pallino@mail.it"];
+//
+// var mailUtente = prompt('inserisci la tua mail');
+// // console.log(mailUtente)
+//
+// document.getElementById('messaggio').innerHTML= "accesso non consentito"
+//
+// for (var i=0; i <= (listaMail.length -1); i++)
+// {
+//     if (mailUtente === listaMail[i]) {
+//         document.getElementById('messaggio').innerHTML= "accesso consentito"
+//     }
+// }
 
 
 //
@@ -33,3 +33,18 @@ for (var i=0; i <= (listaMail.length -1); i++)
 // 3. si ma noi cosa vogliamo fare?
 // 4. torniamo a scrivere in italiano
 // 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+var numeroUtente = Math.floor(Math.random()*6)+1;
+var numeroComputer = Math.floor(Math.random()*6)+1;
+var messaggio;
+if (numeroUtente > numeroComputer) {
+    messaggio = "Hai vinto!"
+} else if (numeroUtente < numeroComputer) {
+    messaggio = "Il computer ha vinto :("
+} else {
+    messaggio = "Pareggio!"
+}
+
+document.getElementById('dado-utente').innerHTML = numeroUtente;
+document.getElementById('dado-computer').innerHTML = numeroComputer;
+document.getElementById('risultato').innerHTML = messaggio;
